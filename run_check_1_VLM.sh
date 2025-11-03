26d47857c873f00c17aa42447b6a443d55bc4db3
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-11-03 21:47:17 +0100

    changing path test

--- run_check_1_VLM.sh
+++ run_check_1_VLM.sh
@@ -1,2 +1,5 @@
+# CUDA_VISIBLE_DEVICES=0,1 PYTHONPATH='./' python eval/test_benchmark.py --model_name "vila-1.5-8b" \
+#     --dataset_path /data0/sebastian.cavada/compositional-physics/tiny_vqa_creation/output --split val
+
 CUDA_VISIBLE_DEVICES=0,1 PYTHONPATH='./' python eval/test_benchmark.py --model_name "vila-1.5-8b" \
-    --dataset_path /data0/sebastian.cavada/compositional-physics/tiny_vqa_creation/output --split val
+    --dataset_path /mnt/proj1/eu-25-92/tiny_vqa_creation/output --split val
