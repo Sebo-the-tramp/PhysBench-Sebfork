3ab82b33fd263d8d88a91b1dd424a6395ff6109c
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2025-12-21 16:54:39 +0100

    adding slurm small

---
+++ run_slurm_small.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A EU-25-92
+#SBATCH -p qgpu
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=8
+#SBATCH -t 12:00:00
+#SBATCH -J interactive_gpu
+
+source ".telegram_bot.env"
+
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
+     -d chat_id="${TELEGRAM_CHAT_ID}" \
+     --data-urlencode text="🚀 GPU session started for GENERAL_SMALL_MODELS on $(hostname) at $(date)" >/dev/null &
+
+source /mnt/proj1/eu-25-92/physbench/.venv/bin/activate
+
+python small_run_parallel.py
+
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
+     -d chat_id="${TELEGRAM_CHAT_ID}" \
+     --data-urlencode text="✅ GPU session completed for GENERAL_SMALL_MODELS on $(hostname) at $(date)" >/dev/null &
