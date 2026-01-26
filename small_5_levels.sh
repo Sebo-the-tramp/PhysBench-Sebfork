e485a5abae6dde3bd0975557f92be04a08525d4d
sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2026-01-26 08:43:46 +0100

    changing sbatch 5 levels to put in queue

--- small_5_levels.sh
+++ small_5_levels.sh
@@ -3,7 +3,7 @@
 #SBATCH -p qgpu
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=8
-#SBATCH -t 12:00:00
+#SBATCH -t 4:00:00
 #SBATCH -J interactive_gpu
 
 source "/home/it4i-thvu/seb_dev/.telegram_bot.env"
@@ -14,7 +14,7 @@ curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
 
 source /mnt/proj1/eu-25-92/physbench/.venv/bin/activate
 
-RUN_NAME="run_11_general_levels"
+RUN_NAME="run_24_general_levels"
 QUANTITY="5K"
 MODEL_SIZE="small"
 
