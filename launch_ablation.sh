906bc7766ccac514f366974c409201baae7bdb3a
Sebastian Cavada <sebastian.cavada.lab@gmail.com>
2026-02-09 21:28:25 +0100

    fixing launches ablations

--- launch_ablation.sh
+++ launch_ablation.sh
@@ -2,13 +2,16 @@
 set -euo pipefail
 
 jobs=(
-  "small_roi_baseline_run_slurm.sh"
+  # "small_roi_baseline_run_slurm.sh"
   "small_roi_no_text_run_slurm.sh"
-  "small_roi_text_run_slurm.sh"
+  # "small_roi_text_run_slurm.sh"
   "small_roi_no_text_layout_position_run_slurm.sh"
   "small_roi_text_layout_position_run_slurm.sh"
-  "small_no_roi_circling_no_text_yes_layout_position_run_slurm.sh"
-  "small_no_roi_circling_yes_text_yes_layout_position_run_slurm.sh"
+  # "small_no_roi_circling_no_text_yes_layout_position_run_slurm.sh"
+  # "small_no_roi_circling_yes_text_yes_layout_position_run_slurm.sh"
+
+  "small_ablation_mass.sh",
+  "small_ablation_duration.sh"
 )
 
 RUN_NUMBER="${1:-${RUN_NUMBER:-}}"
