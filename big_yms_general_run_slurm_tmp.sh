9baaf75e0391effb462beef4af7117beb3925a68
Sebastian Cavada <sebastian.cavada.lab@gmail.com>
2026-02-28 23:41:43 +0100

    updating big yms too

--- big_yms_general_run_slurm_tmp.sh
+++ big_yms_general_run_slurm_tmp.sh
@@ -2,7 +2,7 @@
 #SBATCH -A EU-25-92
 #SBATCH -p qgpu
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=6
+#SBATCH --gpus-per-node=8
 #SBATCH -t 12:00:00
 #SBATCH -J interactive_gpu
 
@@ -18,7 +18,7 @@ curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
      --data-urlencode text="🚀 ${SCRIPT_NAME} started on $(hostname) at $(date) | RUN_NAME=${RUN_NAME} | QUANTITY=${QUANTITY} | MODEL_SIZE=${MODEL_SIZE}" >/dev/null &
 source /mnt/proj1/eu-25-92/physbench/.venv/bin/activate
 
-python run_parallel.py \
+python run_parallel_tmp.py \
     --model-size "${MODEL_SIZE}" \
     --run-name "${RUN_NAME}" \
     --quantity "${QUANTITY}"
